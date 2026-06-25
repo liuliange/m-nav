8616f3f4d948317185c7dd4de989bfcd81023de3
榴莲哥 <949995568@qq.com>
2026-06-25 21:10:09 +0800

    Update logo.tsx

--- apps/web/components/logo.tsx
+++ apps/web/components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import logoImage from '@/assets/images/logo.jpg'
+import logoImage from '@/assets/images/logo.png'
 
 export const Logo = ({ className }: { className?: string }) => {
   return <Image src={logoImage} alt="Logo" className={className} />
